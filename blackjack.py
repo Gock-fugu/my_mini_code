bece02d2a4a06a59b34af58948dfc753e6085d3e
Gock-fugu <99351815+Gock-fugu@users.noreply.github.com>
2023-06-09 14:33:32 +0300

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,41 +1,34 @@
 import random, itertools
+Decks=random.sample([''.join(card) for card in itertools.product([x for n in ([str(i) for i in range(2, 11)],['J', 'Q', 'K', 'A']) for x in n], ['♠️', '♣️', '♥️', '♦️'])]*6, 312)
+
 class BlackJack():
-    def __init__(self, leave_cards=[], Player=[], points_players=[]):
+    def __init__(self, leave_cards=[], Player=[], Diler=[]):
         self.leave_cards=leave_cards
         self.Player=Player
-        self.points_players=points_players
+        self.Diler=Diler
     
     def Play(self):
-        Decks=random.sample([''.join(card) for card in itertools.product([x for n in ([str(i) for i in range(2, 11)],['J', 'Q', 'K', 'A']) for x in n], ['♠️', '♣️', '♥️', '♦️'])], 52)
-        while True:
-            question=input("Готові зіграти в очко? Пиши тільки yes або no.")
-            if question.lower() == 'yes':
-                p=1
-                players=int(input("Скілька гравців будуть грати?"))
-                while p <= players:
-                    print(f"{p}-й гравець тягне карти")
-                    question_2=input("Витягуєш карту?")
-                    if question_2.lower() == 'yes':
-                        a=random.choice(Decks)
-                        print(a)
-                        self.leave_cards.append(a)
-                        self.Player.append(int(a[0] if a[0].isdigit() else(10 if a[0] in ['J', 'Q', 'K'] else 11)))
-                    elif question_2.lower() == 'no':
-                         
-                        print(f"{p}-й гравець набрав: {sum(self.Player)}")
-                        self.points_players.append(sum(self.Player) if sum(self.Player)<21 else (sum(self.Player)-10 if 11 in self.Player else sum(self.Player)))
-                        self.Player.clear()
-                        p+=1
-            elif question.lower() == 'no':
-                break
-            BlackJack().WINNER(), BlackJack().CLS()
-            
-    def WINNER(self):
-        self.points_players= [p if p<=21 else p*-1 for i, p in enumerate(self.points_players)]
-        print(f"переміг {self.points_players.index(max(self.points_players))+1} гравець" if sum(self.points_players)>0 else "Ви всі лохи")
-        print(self.points_players)        
-        
-    def CLS(self):
-        self.points_players.clear(), self.leave_cards.clear()
-
-BlackJack().Play()
+        players = int(input("Скілька гравців будуть грати?"))
+        for i in range(2):
+            self.Diler.append(Decks.pop(0))
+        print(self.Diler [0])
+        for i in range(2):
+            self.Player.append(Decks.pop(0))
+        print(self.Player)
+        player = 1
+        while player <= players:
+            while True:
+                HIT = input("Hit?")
+                if HIT.lower() == 'yes':
+                    self.Player.append(Decks.pop(0))
+                    print(self.Player)
+                    
+                elif HIT.lower() == 'no':
+                    player+=1
+                    self.Diler.append(Decks.pop(0)) if sum([int(c[0]) if c[0].isdigit() else (10 if c.count('10' or 'J' or 'Q' or 'K')==1 else 11) for c in self.Diler])<17 else None
+                    print(self.Player, self.Diler)
+                    print([int(c[0]) if c[0].isdigit() else (10 if '10' or 'J' or 'Q' or 'K' in c else 11) for c in self.Player], [int(c[0]) if c[0].isdigit() else (10 if '10' or 'J' or 'Q' or 'K' in c else 11) for c in self.Diler])
+                    self.Player.clear(), self.Diler.clear()
+                    break
+while True:    
+    BlackJack().Play()
