b2afc0c79766bf2c51feca8f6758d447cb9ffe4d
Gock-fugu <99351815+Gock-fugu@users.noreply.github.com>
2023-06-13 15:54:54 +0300

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -9,26 +9,22 @@ class BlackJack():
     
     def Play(self):
         players = int(input("Скілька гравців будуть грати?"))
-        for i in range(2):
-            self.Diler.append(Decks.pop(0))
-        print(self.Diler [0])
-        for i in range(2):
-            self.Player.append(Decks.pop(0))
-        print(self.Player)
-        player = 1
-        while player <= players:
+        for player in range(players):
+            for i in range(2):
+                self.Diler.append(Decks.pop(0))
+                self.Player.append(Decks.pop(0))
+            print(f"{self.Player}\n{self.Diler[0]}")
             while True:
                 HIT = input("Hit?")
                 if HIT.lower() == 'yes':
                     self.Player.append(Decks.pop(0))
                     print(self.Player)
                     
-                elif HIT.lower() == 'no':
+                elif HIT.lower() == 'no':#теба тробити віднімання очків при умові що сума очків більша 21 і наявність туза в рукаві
                     player+=1
                     self.Diler.append(Decks.pop(0)) if sum([int(c[0]) if c[0].isdigit() else (10 if c.count('10' or 'J' or 'Q' or 'K')==1 else 11) for c in self.Diler])<17 else None
-                    print(self.Player, self.Diler)
-                    print([int(c[0]) if c[0].isdigit() else (10 if '10' or 'J' or 'Q' or 'K' in c else 11) for c in self.Player], [int(c[0]) if c[0].isdigit() else (10 if '10' or 'J' or 'Q' or 'K' in c else 11) for c in self.Diler])
+                    print(f"{self.Player}, {self.Diler}\n {[int(c[0]) if c[0].isdigit() else (10 if '10' or 'J' or 'Q' or 'K' in c else 11) for c in self.Player]}, {[int(c[0]) if c[0].isdigit() else (10 if '10' or 'J' or 'Q' or 'K' in c else 11) for c in self.Diler]}")
                     self.Player.clear(), self.Diler.clear()
                     break
-while True:    
+for i in range(1000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_0000):    
     BlackJack().Play()
