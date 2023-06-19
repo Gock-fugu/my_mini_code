7ebecf3aada39a3c74fe2f6a315bfc19d9b16747
Gock-fugu <99351815+Gock-fugu@users.noreply.github.com>
2023-06-19 14:53:24 +0300

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,4 @@
-import random, itertools
+import random, itertools, time, sqlite3
 Decks=random.sample([''.join(card) for card in itertools.product([x for n in ([str(i) for i in range(2, 11)],['J', 'Q', 'K', 'A']) for x in n], ['♠️', '♣️', '♥️', '♦️'])]*6, 312)
 
 class BlackJack():
@@ -6,9 +6,29 @@ class BlackJack():
         self.Player=Player
         self.Diler=Diler
     
+    def REGISTER(self, Player_list=None):
+        if Player_list is None:
+            Player_list = []
+        conn = sqlite3.connect('Coins_player.db')
+        cursor=conn.cursor()
+        while len(Player_list)!=5:
+            Name=input("Впиши ім'я:")
+            if Name.lower()=='no':
+                break
+            else:
+                cursor.execute("SELECT COUNT(*) FROM Coins WHERE name = ?", (Name,))
+                if cursor.fetchone()[0] == 0:
+                    cursor.execute("INSERT INTO Coins (name, price) VALUES (?, ?)", (Name, 500)) 
+                    conn.commit()
+                Player_list.update(Name)
+        conn.close()
+        return Player_list
+
     def Play(self):
-        players = int(input("Скілька гравців будуть грати?"))
-        for player in range(players):
+        Player_list=self.REGISTER()
+        print(Player_list)
+        for player in Player_list:
+            print(f'Зараз грає {player}')
             for i in range(2):
                 self.Diler.append(Decks.pop(0))
                 self.Player.append(Decks.pop(0))
@@ -19,12 +39,13 @@ class BlackJack():
                     self.Player.append(Decks.pop(0))
                     print(self.Player)
                     
-                elif HIT.lower() == 'no':#теба тробити віднімання очків при умові що сума очків більша 21 і наявність туза в рукаві
-                    player+=1
-                    self.Diler.append(Decks.pop(0)) if sum([int(c[0]) if c[0].isdigit() else (10 if c.count('10' or 'J' or 'Q' or 'K')==1 else 11) for c in self.Diler])<17 else None
-                    print(f"{self.Player}, {self.Diler}\n {[int(c[0]) if c[0].isdigit() else (10 if '10' or 'J' or 'Q' or 'K' in c else 11) for c in self.Player]}, {[int(c[0]) if c[0].isdigit() else (10 if '10' or 'J' or 'Q' or 'K' in c else 11) for c in self.Diler]}")
+                elif HIT.lower() == 'no':#треба зробити віднімання очків при умові що сума очків більша 21 і наявність туза в рукаві
+                    self.Diler.append(Decks.pop(0)) if sum([int(c[0]) if (c[0].isdigit() and int(c[0])>1) else (10 if ('10' in c or 'J' in c or 'Q' in c or 'K' in c) else 11) for i, c in enumerate(self.Diler)]) < 17 else None
+                    print(f"{self.Player}, {self.Diler}\n {sum([int(c[0]) if (c[0].isdigit() and int(c[0])>1) else (10 if ('10' in c or 'J' in c or 'Q' in c or 'K' in c) else 11) for i, c in enumerate(self.Player)])}, {sum([int(c[0]) if (c[0].isdigit() and int(c[0])>1) else (10 if ('10' in c or 'J' in c or 'Q' in c or 'K' in c) else 11) for i, c in enumerate(self.Diler)])}")
+                    time.sleep(2)
                     self.Player.clear(), self.Diler.clear()
                     break
+        Player_list.clear()
 
-for i in range(1000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_0000):    
+for i in range(1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_0000):    
     BlackJack().Play()
