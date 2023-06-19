cdeb10cc8b5d8c9160562d9db39cb3fcd37798cc
Gock-fugu <99351815+Gock-fugu@users.noreply.github.com>
2023-06-19 14:54:36 +0300

    Update arif.py

--- arif.py
+++ arif.py
@@ -4,15 +4,10 @@ def arif():
             a=int(input("впиши число а:"))
             b=int(input("впиши число b:"))
             i=input("Яку дію виконати")
-        
+            print(f'{a}+{b}={a+b}' if i=='+' else (f'{a}-{b}={a-b}' if i=='-' else (f'{a}*{b}={a*b}' if i=='*' else (f'{a}/{b}={a/b}' if i=='/' else None))))
         except ValueError:
             pass
-        try:
-            print(f'{a}+{b}={a+b}' if i=='+' else (f'{a}-{b}={a-b}' if i=='-' else (f'{a}*{b}={a*b}' if i=='*' else (f'{a}/{b}={a/b}' if i=='/' else None))))
-
+        
         except ZeroDivisionError:
             print("На 0 не ділиться")
-        
-            
-
 arif()
